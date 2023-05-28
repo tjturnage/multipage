974a5396c6c21d2b8d4e7edd32dbe5ec74e84012
TJ Turnage <t.j.turnage@gmail.com>
2023-05-28 12:39:49 -0400

    - afd.py update

--- pages/afds.py
+++ pages/afds.py
@@ -1,26 +1,35 @@
-# Import necessary libraries 
+# Import necessary libraries
 import dash
-from dash import html #, dcc, callback, Input, Output
+from dash import html  # , dcc, callback, Input, Output
 import dash_bootstrap_components as dbc
 
 dash.register_page(__name__,
-    path='/afds',
-    title='Area Forecast Discussions',
-    name='Area Forecast Discussions')
+                   path='/afds',
+                   title='Area Forecast Discussions',
+                   name='Area Forecast Discussions')
 
 # Define the page layout
+
+
 def layout():
+    """
+    An htnl object that defines the layout of the page.
+
+    Returns:
+        None.
+    """
     return dbc.Container([
         dbc.Row([
             dbc.Col(
                 html.Div([
-                    #dbc.Button("Show File Content", id="display-file-content-btn", color="success", style={'padding':'1em','width':'100%'}),
-                    html.Div(children=[html.ObjectEl(data="https://tjturnage.pythonanywhere.com/multipage/assets/afds.txt")],id="display-file-content-response")
+                    # dbc.Button("Show File Content", id="display-file-content-btn", color="success", style={'padding':'1em','width':'100%'}),
+                    html.Div(children=[html.ObjectEl(
+                        data="https://tjturnage.pythonanywhere.com/multipage/assets/afds.txt")], id="display-file-content-response")
                 ],
-                style={'padding':'1em'})
+                    style={'padding': '1em'})
             )
-        ],style={'padding':'0.5em'}),
-        ])
+        ], style={'padding': '0.5em'}),
+    ])
 
 # ----------------------------------------
 #        Show Text output window
@@ -29,13 +38,13 @@ def layout():
 # The html default for object element width is way too small.
 # Thus, there is a "assets/object.css" file that overrides the defaults
 
-#@callback(
+# @callback(
 #    Output("display-file-content-response", "children"),
 #    Input("display-file-content-btn","n_clicks"),
 #    prevent_initial_call=True,
-#)
+# )
 # The html default for object element width is way too small.
 # Thus, there is a "assets/object.css" file that overrides the defaults
 
-#def show_file_content(n_clicks):
+# def show_file_content(n_clicks):
 #    return [html.ObjectEl(data="https://tjturnage.pythonanywhere.com/assets/afds.txt")]
